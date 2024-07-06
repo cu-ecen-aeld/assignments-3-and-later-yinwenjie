9f00feba410f0a3ba6755ad5414576d4294b7731
yinwenjie <yinwenjie-1@163.com>
2024-07-06 14:05:44 +0800

    update busybox repo

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -62,7 +62,7 @@ mkdir -p home/conf
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
-git clone git://busybox.net/busybox.git
+git clone https://github.com/mirror/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
