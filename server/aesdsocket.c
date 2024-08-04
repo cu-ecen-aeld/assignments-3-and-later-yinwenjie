f9bd022ada9b2c34a8caa3a769c3f2d3eaf6eb04
yinwenjie <yinwenjie-1@163.com>
2024-08-04 22:13:23 +0800

    complete assignment 5 part 1

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -44,42 +44,27 @@ void setup_signal_handler() {
     signal(SIGTERM, handle_signal);
 }
 
-int run_as_daemon() {
+int execute_in_daemon() {
     pid_t pid = fork();
     if(pid < 0) {
-        perror("fork");
-        return 1;
-    }
-
-    if(pid > 0) {
+        syslog(LOG_ERR, "fork process failed");
+        exit(1);
+    } else if(pid > 0) {
         exit(EXIT_SUCCESS);
+    } else if (pid == 0) {
+        syslog(LOG_INFO, "Child process.");
+        if(setsid() == -1) {
+            perror("setsid");
+            return 1;
+        }
     }
-
-    if(setsid() < 0) {
-        perror("setsid");
-        return 1;
-    }
-
-    if(chdir("/") == -1) {
-        return 1;
-    }
-
-    int i;
-    for(i  = 0; i < sysconf(_SC_OPEN_MAX); i++) {
-        close(i);
-    }
-
-    open("/dev/null",O_RDWR);
-    dup(0);
-    dup(0);
-
     return 0;    
 }
 
 int main(int argc, char *argv[]) {
     int daemon_mode = 0;
 
-    /* get option */
+    // get options to check daemon mode
     int c;
     while((c = getopt(argc, argv, "d")) != -1) {
         switch(c){
@@ -91,15 +76,16 @@ int main(int argc, char *argv[]) {
                    exit(1);
         }
     }
-
     if (daemon_mode) {
         printf("daemon mode.\n");
     } else {
         printf("normal mode.\n");
     }
 
-    openlog(NULL, 0, LOG_USER);
+    // Open syslog
+    openlog("aesdsocket", LOG_PID, LOG_USER);
     
+    // Setup signal handler
     setup_signal_handler();
 
     // set up addrinfo hints and node
@@ -110,16 +96,17 @@ int main(int argc, char *argv[]) {
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
+    // get server socket info
     if (getaddrinfo(NULL, PORT, &hints, &servinfo) != 0) {
         syslog(LOG_ERR, "Getaddrinfo failed");
         return -1;
     }
 
+    // Create server socker & set option
     if((server_socket = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) == -1) {
         syslog(LOG_ERR, "failed to create socket");
         return -1;
     }
-
     if(setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
         syslog(LOG_ERR, "Setsockopt failed");
         close(server_socket);
@@ -127,26 +114,32 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
+    // Bind socket to port
     if(bind(server_socket,servinfo->ai_addr, servinfo->ai_addrlen) == -1) {
         close(server_socket);
         syslog(LOG_ERR, "bind socket failed");
         return -1;
     }
 
+    // start as daemon
     if(daemon_mode) {
-        printf("\nBecome daemon function\n");
-        if(daemon(0,0))
-           perror("daemon");
+        printf("Run in daemon mode:\n");
+        if(execute_in_daemon()) {
+            syslog(LOG_ERR, "execute in daemon mode failed");
+            return -1;
+        }
     }
      
     freeaddrinfo(servinfo);
 
+    // Listen for connections
     if(listen(server_socket, BACKLOG) == -1) {
         syslog(LOG_ERR, "Listen failed");
         close(server_socket);
         return -1;
     }
 
+    // Main loop to accept connections
     while (1) {
         struct sockaddr_storage cli_addr;
         socklen_t clilen = sizeof(cli_addr);
@@ -166,10 +159,12 @@ int main(int argc, char *argv[]) {
             addr = &(s->sin6_addr);
         }
 
+        // Log accepted connection
         inet_ntop(cli_addr.ss_family, addr, client_ip, sizeof(client_ip));
         printf("Accepted connection from %s\n", client_ip);
         syslog(LOG_INFO, "Accepted connection from %s", client_ip);
 
+        // Open log file
         if((file = fopen(FILE_PATH, "a+")) == NULL) {
             syslog(LOG_ERR,"File open failed");
             close(client_socket);
@@ -205,13 +200,24 @@ int main(int argc, char *argv[]) {
                 data_len = 0;
             }
         }
-        free(data);
-        fclose(file);
-        file = NULL;
-        close(client_socket);
-        client_socket = -1;
+
+        // free resources
+        if (data != NULL) {
+            free(data);
+            data = NULL;
+        }        
+        if (file != NULL) {
+            fclose(file);
+            file = NULL;
+        }
+        if (client_socket != -1) {
+            close(client_socket);
+            client_socket = -1;
+        }
         syslog(LOG_INFO, "Closed connection from %s", client_ip);
     }
 
+    // Close server socket and syslog
+    closelog();
     return 0;
 }
