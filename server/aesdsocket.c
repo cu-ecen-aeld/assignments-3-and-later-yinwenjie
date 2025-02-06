6b93e8903ca61895770ec23a85ab184b5c4ccbe0
yinwenjie <yinwenjie-1@163.com>
2025-02-06 15:45:33 +0800

    Third attemp for assignment 8

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -11,240 +11,275 @@
 #include <stdbool.h>
 #include <linux/fs.h>
 #include <errno.h>
+#include <syslog.h>
+#include <pthread.h>
 
-#include "aesd_thread.h"
+#include "aesd_thread.h"   // Contains threadfunc, thread_data_t definitions
 #include "queue.h"
 
 #define PORT "9000"
 #define FILE_PATH "/var/tmp/aesdsocketdata"
 #define BACKLOG 10
 
+// Comment out the next line to revert to using the file instead of /dev/aesdchar
 #define USE_AESD_CHAR_DEVICE 1
 
 #ifndef USE_AESD_CHAR_DEVICE
+// Use a global FILE pointer only for the non-device version
 FILE *file = NULL;
 #else
-int file;
+// For the device version, we do NOT hold a global descriptor open.
+// Instead, we open/close inside each function that needs it.
 #endif
 
 int server_socket = -1;
 int client_socket = -1;
 
-static void timer_thread(union sigval sigval) {
-    char* time_fmt = "%a, %d %b %Y %T %z";
-    struct timespec ts;
-    int rc = clock_gettime(CLOCK_REALTIME, &ts);
-    if( rc != 0 ) {
-        fprintf(stderr, "Error %d (%s) getting clock %d (%s) time",
-                errno,strerror(errno),CLOCK_REALTIME,"CLOCK_REALTIME");
-        return;
-    } else {
-        syslog(LOG_DEBUG, "Clock realtime %ld.%09ld",ts.tv_sec,ts.tv_nsec);
-    }
-    char timeStr[32];
-    char buffer[128];
-    memset(timeStr, 0, sizeof(timeStr));
-    struct tm tm;
-    if ( gmtime_r(&ts.tv_sec,&tm) == NULL ) {
-        fprintf(stderr, "Error calling gmtime_r with time %ld",ts.tv_sec);
-    } else  {
-        if (strftime(timeStr, sizeof(timeStr), time_fmt, &tm) == 0) {
-            fprintf(stderr, "strftime returned 0");
-            exit(EXIT_FAILURE);
-        }
-    }
-    thread_data_t* td = (thread_data_t*) sigval.sival_ptr;
-    if ( pthread_mutex_lock(td->pMutex) != 0 ) {
-        fprintf(stderr, "Error %d (%s) locking thread data!\n",errno,strerror(errno));
-    } else {        
-        sprintf(buffer, "timestamp:%s\n", timeStr);
-        syslog(LOG_DEBUG, "Write to file: %s", buffer);
-#ifndef USE_AESD_CHAR_DEVICE
-        fseek(file, 0, SEEK_END); // move pointer to the end of the file
-        fwrite(buffer, 1, strlen(buffer), file);
-#else
-        lseek(file, 0, SEEK_END); // move pointer to the end of the device
-        if(write(file, buffer, strlen(buffer)) < 0) {
-            perror("Error writing to character device");
-        }
-#endif
-        if ( pthread_mutex_unlock(td->pMutex) != 0 ) {
-            fprintf(stderr, "Error %d (%s) unlocking thread data!\n",errno,strerror(errno));
-        }
-    }
-}
-
-void handle_signal(int signal) {
+// ------------------------------------------------------
+// Signal Handling
+// ------------------------------------------------------
+void handle_signal(int signal)
+{
     syslog(LOG_INFO, "Caught signal %d, exiting", signal);
+
     if (client_socket != -1) {
         close(client_socket);
     }
     if (server_socket != -1) {
         close(server_socket);
     }
+
 #ifndef USE_AESD_CHAR_DEVICE
-    if(file != NULL) {
+    // Only close/remove the regular file if we are using it
+    if (file) {
         fclose(file);
+        file = NULL;
     }
     remove(FILE_PATH);
 #else
-    if(file >= 0) {
-        close(file);
-    }
+    // In device mode, nothing special to close here 
+    // because we only open/close the device locally.
 #endif
+
     closelog();
     exit(0);
 }
 
-void setup_signal_handler() {
+void setup_signal_handler()
+{
     signal(SIGINT, handle_signal);
     signal(SIGTERM, handle_signal);
 }
 
-int execute_in_daemon() {
+// ------------------------------------------------------
+// Daemon Setup
+// ------------------------------------------------------
+int execute_in_daemon()
+{
     pid_t pid = fork();
-    if(pid < 0) {
+    if (pid < 0) {
         syslog(LOG_ERR, "fork process failed");
         exit(1);
-    } else if(pid > 0) {
+    } else if (pid > 0) {
         exit(EXIT_SUCCESS);
-    } else if (pid == 0) {
-        syslog(LOG_INFO, "Child process.");
-        if(setsid() == -1) {
+    } else {
+        // Child process
+        if (setsid() == -1) {
             perror("setsid");
             return 1;
         }
     }
-    return 0;    
+    return 0;
 }
 
-int main(int argc, char* argv[]) {
-    int daemon_mode = 0;
+// ------------------------------------------------------
+// Timer Thread Callback
+// ------------------------------------------------------
+static void timer_thread(union sigval sigval)
+{
+    char* time_fmt = "%a, %d %b %Y %T %z";
+    struct timespec ts;
+    int rc = clock_gettime(CLOCK_REALTIME, &ts);
+    if(rc != 0) {
+        fprintf(stderr, "Error %d (%s) getting clock time\n", errno, strerror(errno));
+        return;
+    }
 
-    // 1.Get options to check daemon mode
+    char timeStr[32];
+    memset(timeStr, 0, sizeof(timeStr));
+    struct tm tm;
+
+    if (gmtime_r(&ts.tv_sec, &tm) == NULL) {
+        fprintf(stderr, "Error calling gmtime_r with time %ld\n", ts.tv_sec);
+        return;
+    }
+    if (strftime(timeStr, sizeof(timeStr), time_fmt, &tm) == 0) {
+        fprintf(stderr, "strftime returned 0\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[128];
+    snprintf(buffer, sizeof(buffer), "timestamp:%s\n", timeStr);
+
+    thread_data_t* td = (thread_data_t*) sigval.sival_ptr;
+    if (pthread_mutex_lock(td->pMutex) != 0) {
+        fprintf(stderr, "Error %d (%s) locking thread data!\n", errno, strerror(errno));
+        return;
+    }
+
+    syslog(LOG_DEBUG, "Write to file: %s", buffer);
+
+#ifndef USE_AESD_CHAR_DEVICE
+    // Seek end, write to the local file
+    fseek(file, 0, SEEK_END);
+    fwrite(buffer, 1, strlen(buffer), file);
+#else
+    // Open device, seek end, write, then close
+    int dev_fd = open("/dev/aesdchar", O_RDWR);
+    if (dev_fd < 0) {
+        perror("Error opening /dev/aesdchar");
+    } else {
+        // Seek to end
+        if (lseek(dev_fd, 0, SEEK_END) == (off_t)-1) {
+            perror("Error seeking device file");
+        }
+        ssize_t wr = write(dev_fd, buffer, strlen(buffer));
+        if (wr < 0) {
+            perror("Error writing to character device");
+        }
+        close(dev_fd);
+    }
+#endif
+
+    if (pthread_mutex_unlock(td->pMutex) != 0) {
+        fprintf(stderr, "Error %d (%s) unlocking thread data!\n", errno, strerror(errno));
+    }
+}
+
+// ------------------------------------------------------
+// Main
+// ------------------------------------------------------
+int main(int argc, char* argv[])
+{
+    // Parse args for -d (daemon) flag
+    int daemon_mode = 0;
     int c;
-    while((c = getopt(argc, argv, "d")) != -1) {
-        switch(c){
-               case 'd':
-                   daemon_mode = 1; 
-                   break;
-                default:
-                   printf("Please check option\n");
-                   exit(1);
+    while ((c = getopt(argc, argv, "d")) != -1) {
+        switch (c) {
+            case 'd':
+                daemon_mode = 1;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-d]\n", argv[0]);
+                exit(1);
         }
     }
+
     if (daemon_mode) {
-        // start as daemon
-        printf("Running in daemon mode:\n");
-        if(execute_in_daemon()) {
+        // Become a daemon
+        if (execute_in_daemon()) {
             syslog(LOG_ERR, "execute in daemon mode failed");
             return -1;
         }
-    } else {
-        printf("Running in normal mode.\n");
     }
 
     openlog(NULL, 0, LOG_USER);
-    syslog(LOG_DEBUG, "Starting aesdsocket daemon mode: %d", daemon_mode);
+    syslog(LOG_DEBUG, "Starting aesdsocket, daemon mode: %d", daemon_mode);
 
-    // 2.Setup signal handler
+    // Setup signals
     setup_signal_handler();
 
-    int status;
-    int opt = 1;
-    int server_socket = -1;
-    int client_socket = -1;
-
-    // 3.Set up addrinfo hints and node
+    // --------------------------------------------------
+    // Set up socket
+    // --------------------------------------------------
     struct addrinfo hints, *servinfo = NULL;
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
-    if ((status = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status)); // gai_strerror only for error code of getaddrinfo function
+
+    if (getaddrinfo(NULL, PORT, &hints, &servinfo) != 0) {
+        fprintf(stderr, "getaddrinfo error\n");
         exit(-1);
     }
-    
-    // 4.Create server socker & set option
-    server_socket = socket(servinfo->ai_family, servinfo->ai_socktype|SOCK_NONBLOCK|SOCK_CLOEXEC, servinfo->ai_protocol);
-    if (server_socket == 0) {
+
+    server_socket = socket(servinfo->ai_family, servinfo->ai_socktype | SOCK_NONBLOCK | SOCK_CLOEXEC,
+                           servinfo->ai_protocol);
+    if (server_socket < 0) {
         fprintf(stderr, "could not create socket\n");
-        exit (-1);
-    } 
+        exit(-1);
+    }
+
+    int opt = 1;
     if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(int)) == -1) {
         perror("setsockopt");
         exit(1);
     }
 
-    // 5. Bind the socket 
-    if ((status = bind(server_socket, servinfo->ai_addr, servinfo->ai_addrlen)) != 0) {
+    if (bind(server_socket, servinfo->ai_addr, servinfo->ai_addrlen) != 0) {
         perror("bind");
-        fprintf(stderr, "bind socketfd %d error %d\n", server_socket, status);
         close(server_socket);
         exit(-1);
     }
     freeaddrinfo(servinfo);
 
-    // 6. Listen for new connection
-    if ((status = listen(server_socket, BACKLOG)) == -1) {
+    if (listen(server_socket, BACKLOG) == -1) {
         perror("listen");
-        fprintf(stderr, "listen socketfd %d error %d\n", server_socket, status);
         close(server_socket);
         exit(-1);
-    } 
-    syslog(LOG_DEBUG, "server is now listening to port:%s\n", PORT);
+    }
+    syslog(LOG_DEBUG, "Server listening on port: %s", PORT);
 
-    // 7.Open local data file
 #ifndef USE_AESD_CHAR_DEVICE
+    // For file-based mode, open the local data file once
     file = fopen(FILE_PATH, "w+");
-    if(!file) {
+    if (!file) {
         fprintf(stderr, "Cannot open file %s\n", FILE_PATH);
         close(server_socket);
         closelog();
         return 1;
     }
-#else
-    file = open("/dev/aesdchar", O_RDWR);
-    if(file < 0) {
-        perror("Cannot open character device /dev/aesdchar");
+#endif
+
+    // Create a mutex
+    pthread_mutex_t mutex;
+    if (pthread_mutex_init(&mutex, NULL) != 0) {
+        fprintf(stderr, "Cannot create mutex\n");
         close(server_socket);
         closelog();
         return 1;
     }
-#endif
-    // 8.Mutex for the file accessing
-    pthread_mutex_t mutex;
-    if (pthread_mutex_init(&mutex, NULL) != 0) {
-        fprintf(stderr, "Cannot Create Mutex");
-        close(server_socket);
-        closelog(); // close syslog 
-        return 1;
-    } else {
-        syslog(LOG_DEBUG, "Mutex is inited successfully");
-    }
+    syslog(LOG_DEBUG, "Mutex is inited successfully");
 
-    // 9.Create timer and thread for writing timestamp
+    // --------------------------------------------------
+    // Create timer for writing timestamps
+    // --------------------------------------------------
     thread_data_t td;
     memset(&td, 0, sizeof(thread_data_t));
     td.pMutex = &mutex;
-    td.pFile = file;
+#ifndef USE_AESD_CHAR_DEVICE
+    td.pFile = file;  // For local file usage
+#else
+    // For device usage, no persistent file descriptor
+    td.pFile = -1;
+#endif
 
     struct sigevent sev;
     struct itimerspec its;
     timer_t timer_id;
-    int clock_id = CLOCK_MONOTONIC;
     memset(&sev, 0, sizeof(struct sigevent));
     sev.sigev_notify = SIGEV_THREAD;
     sev.sigev_value.sival_ptr = &td;
     sev.sigev_notify_function = timer_thread;
+
+    int clock_id = CLOCK_MONOTONIC;
     if (timer_create(clock_id, &sev, &timer_id) == -1) {
-        fprintf(stderr, "Error %d (%s) creating timer!\n",errno,strerror(errno));
+        fprintf(stderr, "Error %d (%s) creating timer!\n", errno, strerror(errno));
     } else {
-        syslog(LOG_DEBUG, "Successfully setup timer, wait for timer thread to run");
+        syslog(LOG_DEBUG, "Successfully set up timer, wait for timer thread to run");
     }
 
-    // Start the timer to expire after 1 second and then every 1 second
+    // Start the timer: first expiration in 10s, then repeat every 10s
     its.it_value.tv_sec = 10;
     its.it_value.tv_nsec = 0;
     its.it_interval.tv_sec = 10;
@@ -253,81 +288,96 @@ int main(int argc, char* argv[]) {
         perror("timer_settime");
         exit(EXIT_FAILURE);
     }
-    
+
+    // Accept loop...
     struct sockaddr_storage clientAddr;
-    socklen_t clientAddrLen;
-    clientAddrLen = sizeof clientAddr;
+    socklen_t clientAddrLen = sizeof clientAddr;
     char ipstr[INET6_ADDRSTRLEN];
+
+    // A singly-linked list for tracking threads
     SLIST_HEAD(slisthead, slist_thread_s) head;
     SLIST_INIT(&head);
+
     while (1) {
-        // 10. Accepting the connection 
         client_socket = accept(server_socket, (struct sockaddr *)&clientAddr, &clientAddrLen);
-        if (client_socket != -1) {
-            getpeername(client_socket, (struct sockaddr*)&clientAddr, &clientAddrLen);
-            // deal with both IPv4 and IPv6:
+        if (client_socket >= 0) {
+            // IP info
             if (clientAddr.ss_family == AF_INET) {
                 struct sockaddr_in *s = (struct sockaddr_in *)&clientAddr;
                 inet_ntop(AF_INET, &s->sin_addr, ipstr, sizeof ipstr);
-            } else { // AF_INET6
+            } else {
                 struct sockaddr_in6 *s = (struct sockaddr_in6 *)&clientAddr;
                 inet_ntop(AF_INET6, &s->sin6_addr, ipstr, sizeof ipstr);
-            };
-            printf("Accepted connection from %d ipaddr: %s\n", client_socket, ipstr);
+            }
+            printf("Accepted connection from %s\n", ipstr);
             syslog(LOG_DEBUG, "Accepted connection from %s", ipstr);
-            // start the thread for the new connection 
+
+            // Spawn thread to handle the connection
             pthread_t thread;
-            thread_data_t* data = malloc(sizeof(thread_data_t));
+            thread_data_t *data = malloc(sizeof(thread_data_t));
             if (!data) {
-                fprintf(stderr, "Failed to malloc thread_data \n");
-                return false;
+                fprintf(stderr, "Failed to malloc thread_data\n");
+                close(client_socket);
+                continue;
             }
             data->pMutex = &mutex;
             data->isCompleted = false;
+#ifndef USE_AESD_CHAR_DEVICE
             data->pFile = file;
+#else
+            data->pFile = -1; // We'll open/close /dev/aesdchar within the thread
+#endif
             data->clientFd = client_socket;
+
             int rc = pthread_create(&thread, NULL, threadfunc, data);
             data->thread = thread;
             if (rc != 0) {
-                fprintf(stderr, "Failed to create thread for connection from %d ipaddr: %s\n", client_socket, ipstr);
+                fprintf(stderr, "Failed to create thread for client %s\n", ipstr);
+                free(data);
+                close(client_socket);
             } else {
-                // Add the data to thread list
-                slist_thread_t *threaListData = malloc(sizeof(slist_thread_t));
-                threaListData->pThreadData = data;
-                syslog(LOG_DEBUG, "Insert new thread %lu data to list", data->thread);
-                SLIST_INSERT_HEAD(&head, threaListData, entries);
+                // Insert into singly-linked list
+                slist_thread_t *threadListData = malloc(sizeof(slist_thread_t));
+                threadListData->pThreadData = data;
+                SLIST_INSERT_HEAD(&head, threadListData, entries);
             }
         } else {
+            // No new connections, wait briefly
             usleep(100000);
         }
 
-        // 11. Start thread to receieve and send data
-        slist_thread_t *threadListData;
-        slist_thread_t *tempThreadListData;
-        SLIST_FOREACH_SAFE(threadListData, &head, entries, tempThreadListData) {
+        // Check for completed threads
+        slist_thread_t *threadListData = NULL, *tempThreadListData = NULL;
+        SLIST_FOREACH_SAFE(threadListData, &head, entries, tempThreadListData)
+        {
             if (threadListData->pThreadData->isCompleted) {
-                void * thread_rtn = NULL;
-                syslog(LOG_DEBUG, "Thread %lu of client %d is completed", threadListData->pThreadData->thread, threadListData->pThreadData->clientFd);
-                int tryjoin_rtn =  pthread_join(threadListData->pThreadData->thread, &thread_rtn);
+                void *thread_rtn = NULL;
+                int tryjoin_rtn = pthread_join(threadListData->pThreadData->thread, &thread_rtn);
                 if (tryjoin_rtn != 0) {
-                    fprintf(stderr, "Cannot join the thread %lu of client %d", threadListData->pThreadData->thread, threadListData->pThreadData->clientFd);
+                    fprintf(stderr, "Cannot join thread %lu\n", threadListData->pThreadData->thread);
                 } else {
-                    syslog(LOG_DEBUG, "Joined thread %lu of client %d successfully", threadListData->pThreadData->thread, threadListData->pThreadData->clientFd);
+                    syslog(LOG_DEBUG, "Joined thread %lu successfully", threadListData->pThreadData->thread);
                     close(threadListData->pThreadData->clientFd);
-                    SLIST_REMOVE(&head,threadListData, slist_thread_s, entries);
+                    SLIST_REMOVE(&head, threadListData, slist_thread_s, entries);
                     free(threadListData->pThreadData);
                     free(threadListData);
                 }
             }
         }
     }
-    
-    syslog(LOG_DEBUG, "close server socket");
+
+    // Cleanup
+    syslog(LOG_DEBUG, "Shutting down server");
     close(server_socket);
+
+#ifndef USE_AESD_CHAR_DEVICE
     if (file) {
         fclose(file);
         file = NULL;
+        remove(FILE_PATH);
     }
+#endif
+
     closelog();
     return 0;
 }
